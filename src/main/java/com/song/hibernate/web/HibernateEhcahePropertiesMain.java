dc34bf346794b7dae30cbed731c9cc049acaee16
SOYU <songyushi@wondersgroup.com>
2017-01-12 16:37:01 +0800

    验证session级别缓存，验证将启用ehcache缓存配置置于properties文件中

--- src/main/java/com/song/hibernate/web/HibernateEhcahePropertiesMain.java
+++ src/main/java/com/song/hibernate/web/HibernateEhcahePropertiesMain.java
@@ -1,9 +1,7 @@
 package com.song.hibernate.web;
 
 import java.util.List;
-import java.util.Map;
 
-import org.hibernate.Cache;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -14,7 +12,7 @@ import com.song.hibernate.service.model.po.UserInfoPO;
 
 /**
  * <p>
- * Title: hibernate_[二级缓存]_[ehcache]
+ * Title: hibernate_[二级缓存]_[ehcache开启前后]
  * </p>
  * <p>
  * Description: [测试类]
@@ -26,49 +24,43 @@ import com.song.hibernate.service.model.po.UserInfoPO;
  * @since 20100901
  */
 @SuppressWarnings("deprecation")
-public class HibernateEhcaheMain {
+public class HibernateEhcahePropertiesMain {
+
 	public static void main(String[] args) {
-		Configuration conf = new Configuration().configure();
+		Configuration conf = new Configuration().addAnnotatedClass(UserInfoPO.class);
 		ServiceRegistry serviceRegistry =
 				new ServiceRegistryBuilder().applySettings(conf.getProperties()).buildServiceRegistry();
 		SessionFactory sf = conf.buildSessionFactory(serviceRegistry);
 
+		// 在未开启二级缓存情况下，打开第一、二个session会两次发送sql;开启二级缓存情况下，只在第一个session处发送sql
 		// 打开第一个session
 		Session sess = sf.openSession();
 		sess.beginTransaction();
 		@SuppressWarnings({ "rawtypes", "unused" })
 		List list = sess.createQuery(" FROM UserInfoPO userInfo ").list();
 		sess.getTransaction().commit();
-		System.out.println("==========================");
 
 		// 打开第二个session
+		System.out.println("==========打开第二个Session============");
 		Session sess2 = sf.openSession();
 		sess2.beginTransaction();
-		// 根据主键加载实体，系统直接从二级缓存中读取
-		// 因此不会发出查询sql
 		UserInfoPO userInfoPO = (UserInfoPO) sess2.load(UserInfoPO.class, 1);
 		System.out.println(userInfoPO.getUsername());
 		sess2.getTransaction().commit();
 
-		// 测试二级缓存统计功能
-		System.out.println("二级缓存的统计功能");
-		@SuppressWarnings("rawtypes")
-		Map cacheEntries =
-				sf.getStatistics().getSecondLevelCacheStatistics("com.song.hibernate.service.model.po.UserInfoPO")
-						.getEntries();
-		System.out.println(cacheEntries);
-
-		// 测试管理二级缓存功能
-		Cache cache = sf.getCache();
-		// 清除二级缓存中指定的对象
-		cache.evictEntity(UserInfoPO.class, 1);
-
+		// 无论是否开启二级缓存，第一次查询总会发送sql，第二次不会
 		// 打开第三个session
+		System.out.println("==========打开第三个Session============");
 		Session sess3 = sf.openSession();
 		sess3.beginTransaction();
-		// 根据主键加载实体，因为已从二级缓存中移除所以会从数据库中查找并发出查询sql
-		UserInfoPO userInfoPO3 = (UserInfoPO) sess3.load(UserInfoPO.class, 1);
-		System.out.println(userInfoPO3.getUsername());
+		@SuppressWarnings("unchecked")
+		List<UserInfoPO> listPO = (List<UserInfoPO>) sess3.createQuery(" FROM UserInfoPO userInfo ").list();
+		System.out.println(listPO.get(0).getUsername());
+
+		UserInfoPO userInfoPOThree = (UserInfoPO) sess3.load(UserInfoPO.class, 1);
+		System.out.println(userInfoPOThree.getUsername());
 		sess3.getTransaction().commit();
+
 	}
+
 }
